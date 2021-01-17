598e9ea2d5befa50e25358f938ce7366d6350212
giegeralexandra <giegeralexandra@gmail.com>
2021-01-17 16:50:27 -0500

    added signup new view and routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
-class StylistsController < ApplicationController
+class UsersController < ApplicationController
+
+    def new 
+        @user = User.new 
+    end
+
+    def create 
+        @user = User.new(user_params)
+        if @user.save 
+            session[:user_id] = @user.id 
+            redirect_to user_path(@user)
+        else 
+            render :new 
+        end
+    end
+
+    def show 
+    end
+
+    private 
+
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :email, :password)
+    end
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
     has_many :customers
     has_many :appointments
     has_many :categories
+    has_secure_password #authenticate, validate password, password confirmation 
 end
