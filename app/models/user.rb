1831b66aad504e3244ca12a814ba34a6a19e5b44
giegeralexandra <giegeralexandra@gmail.com>
2021-01-23 13:53:16 -0500

    clearned up user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
     has_many :customers
     has_many :appointments
     has_many :categories
-    has_secure_password #authenticate, validate password, password confirmation 
-    validates_uniqueness_of :email
 
+    has_secure_password 
+
+    validates_uniqueness_of :email { case_sensitive: false }
 end
