268bf1d67dfde0299cdbaeed5efa72efb5afa6cd
giegeralexandra <giegeralexandra@gmail.com>
2021-01-23 13:50:24 -0500

    cleaned up category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,12 +2,13 @@ class Category < ApplicationRecord
     belongs_to :user 
     has_many :appointments
     has_many :customers, through: :appointments 
+
     validates :name, presence: true 
-    validates :name, {:length => { :maximum => 12}}
-    validates :name, {:length => { :minimum => 2}}
+    validates :name, {:length => { :maximum => 12, :minimum => 2}}
     validate :uniqueness_of_category_for_user_only 
 
-
+    #validations 
+    
     def uniqueness_of_category_for_user_only
         current_user = User.find_by(id: self.user_id)
         current_user.categories.each do |category|
